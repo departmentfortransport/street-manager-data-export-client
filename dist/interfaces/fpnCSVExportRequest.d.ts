11d0bc770f7ad236f57ad019e8275f3bf54e9b8a
Peadar Kelly <peadark@kainos.com>
2019-12-06 17:23:13 +0000

    Added documentation

--- dist/interfaces/fpnCSVExportRequest.d.ts
+++ dist/interfaces/fpnCSVExportRequest.d.ts
@@ -2,6 +2,8 @@ import { BaseCSVExportRequest } from './baseCSVExportRequest';
 import { FPNStatus } from './referenceTypes';
 export interface FPNCSVExportRequest extends BaseCSVExportRequest {
     status?: FPNStatus[];
+    /** start_date must be before or the same as end_date if both are provided */
     start_date?: Date;
+    /** end_date must be the same as or after start_date if both are provided */
     end_date?: Date;
 }
