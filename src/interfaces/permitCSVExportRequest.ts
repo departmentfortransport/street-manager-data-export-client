0bc429fcc622dbb26416237bc407db190144f57c
Matthew O'Reilly <m.oreilly@kainos.com>
2020-01-29 16:17:42 +0000

    SM-4428: Add new ever_modification_requested property to PermitCSVExportRequest.

--- src/interfaces/permitCSVExportRequest.ts
+++ src/interfaces/permitCSVExportRequest.ts
@@ -20,4 +20,5 @@ export interface PermitCSVExportRequest extends BaseCSVExportRequest {
   is_deemed?: boolean
   lane_rental_charges_not_agreed?: boolean
   lane_rental_charges_potentially_apply?: boolean
+  ever_modification_requested?: boolean
 }
