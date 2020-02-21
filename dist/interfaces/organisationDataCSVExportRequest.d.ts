b9b26d4b64fe2c85e8b2d7b0d5ef640dc461fdb6
Peadar Kelly <peadark@kainos.com>
2020-02-21 14:48:44 +0000

    Renamed request file, added client method

---
+++ dist/interfaces/organisationDataCSVExportRequest.d.ts
@@ -0,0 +1,7 @@
+import { BaseCSVExportRequest } from './baseCSVExportRequest';
+export interface OrganisationDataCSVExportRequest extends BaseCSVExportRequest {
+    start_date: Date;
+    start_date_time?: Date;
+    end_date: Date;
+    end_date_time?: Date;
+}
