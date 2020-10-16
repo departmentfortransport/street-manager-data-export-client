e4bec4932c8c1edfe39426f71a5ab51019e2ca98
Cormac Curran <c.curran@kainos.com>
2020-10-16 14:03:05 +0100

    feature/SM-5153: add status_changed_date filters to FPNCSVExportRequest
    - update FPNCSVExportRequest to include status_changed_date_from & status_changed_date_to

--- dist/interfaces/fpnCSVExportRequest.d.ts
+++ dist/interfaces/fpnCSVExportRequest.d.ts
@@ -7,4 +7,8 @@ export interface FPNCSVExportRequest extends BaseCSVExportRequest {
     /** end_date must be the same as or after start_date if both are provided */
     end_date?: Date;
     offence_code?: OffenceCode[];
+    /** status_changed_date_from must be before or the same as status_changed_date_to if both are provided */
+    status_changed_date_from?: Date;
+    /** end_date must be the same as or after status_changed_date_from if both are provided */
+    status_changed_date_to?: Date;
 }
