0f286c8263c3eb15540d9b47687fb2550e838c00
Cormac Curran <c.curran@kainos.com>
2021-01-21 16:36:39 +0000

    add additional filter params to the ReinstatementCSVExportRequest:
    * registration_date_from
    * registration_date_to
    * end_date_from
    * end_date_to

--- src/interfaces/reinstatementCSVExportRequest.ts
+++ src/interfaces/reinstatementCSVExportRequest.ts
@@ -4,4 +4,8 @@ import { BaseCSVExportRequest } from './baseCSVExportRequest'
 export interface ReinstatementCSVExportRequest extends BaseCSVExportRequest {
   status?: ReinstatementStatus[]
   latest_reinstatements_only?: boolean
+  registration_date_from?: Date
+  registration_date_to?: Date
+  end_date_from?: Date
+  end_date_to?: Date
 }
